1088a001d52e65171eeaf0b4ae3c4b1b806a7a66
lokeshtrvl-crypto <lokeshtrvl@gmail.com>
2026-02-03 16:02:14 +0530

    Setup Vite + React + Tailwind + GSAP

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+const NavBar = () => {
+    return (
+        <header>
+            <nav>
+                <img src="/logo.svg" alt="Apple logo" />
+
+                <ul>
+                    {[
+                        { label: 'Store' },
+                        { label: 'Mac' },
+                        { label: 'Watch' },
+                        { label: 'iPhone' },
+                        { label: 'Vision' },
+                        { label: 'AirPods' },
+                    ].map((link) => (
+                        <li key={link.label}>
+                            <a href={link.label}>{link.label}</a>
+                        </li>
+                    ))}
+                </ul>
+                <div className={"flex-center gap-3"}>
+                    <button>
+                        <img src="/search.svg" alt="Search" />
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="Cart" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
+}
+export default NavBar
